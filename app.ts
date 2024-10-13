69965a4981306589d1ae00ee6f65aa9ae614054f
aviel <aviel469@gmail.com>
2024-10-13 11:36:49 +0300

    add connection to db

--- app.ts
+++ app.ts
@@ -3,20 +3,22 @@ import authRoutes from './src/routes/authRoutes'
 import teacherRoutes from './src/routes/teachersRoutes'
 import studentRoutes from './src/routes/studentsRoutes'
 import dotenv from "dotenv";
+import connectDB from "./src/config/dbConfig";
 
 dotenv.config();
 
 const app :Express = express();
 const PORT = process.env.PORT || 3000;
+connectDB()
 
 // Middleware
 app.use(express.json());
 
 
 // Routes
-app.use("auth" , authRoutes)
-app.use("teachers" , teacherRoutes)
-app.use("students" , studentRoutes)
+app.use("/auth" , authRoutes)
+app.use("/teachers" , teacherRoutes)
+app.use("/students" , studentRoutes)
 
 // app.use()
 
---
+++ src/controllers/authController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+export const Login = async (req: Request, res: Response): Promise<void> => {
+  try {
+  } catch (err) {}
+};
+export const Logout = async (req: Request, res: Response): Promise<void> => {
+  try {
+  } catch (err) {}
+};
