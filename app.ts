34037cd1445b1223944d474feae1283730afd0ca
aviel <aviel469@gmail.com>
2024-10-13 10:59:44 +0300

    build app.ts routers connection

---
+++ app.ts
@@ -0,0 +1,29 @@
+import express,{Express} from "express";
+import authRoutes from './src/routes/authRoutes'
+import teacherRoutes from './src/routes/teachersRoutes'
+import studentRoutes from './src/routes/studentsRoutes'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app :Express = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+
+// Routes
+app.use("auth" , authRoutes)
+app.use("teachers" , teacherRoutes)
+app.use("students" , studentRoutes)
+
+// app.use()
+
+// Error handling middleware
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
